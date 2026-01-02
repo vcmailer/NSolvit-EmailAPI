321f2086d419c79f11248f77d80e0acfaa95c8a8
vcmailer <vcmailerpro@gmail.com>
2026-01-02 13:36:30 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,14 +1,5 @@
 export default {
   fetch(request, env) {
-    const var1 = env.MAILERSEND_API_KEY;
-    const var2 = env.RECAPTCHA_SECRET_KEY;
-
-    if (!var1 || !var2) {
-      return new Response("Missing variables", { status: 500 });
-    }
-
-    return new Response(
-      `Worker active\nMAILERSEND_API_KEY=${var1}\nRECAPTCHA_SECRET_KEY=${var2}`
-    );
+    return new Response("Worker running");
   }
 };
